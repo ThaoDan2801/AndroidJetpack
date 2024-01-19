6f59a72454190229deab2f2227d1d28c0a802a16
ThaoDan2801 <thaoto2801@gmail.com>
2024-01-19 21:42:30 +0700

    handel one time event with live data

--- app/src/main/java/com/glucozo/android_jetpack/view/fragment/OneFragment.kt
+++ app/src/main/java/com/glucozo/android_jetpack/view/fragment/OneFragment.kt
@@ -1,19 +1,26 @@
 package com.glucozo.android_jetpack.view.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.glucozo.android_jetpack.databinding.FragmentDataBinding
+import com.glucozo.android_jetpack.view.MainActivity
 import com.glucozo.android_jetpack.viewmodel.DataViewModel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class OneFragment : Fragment() {
     private lateinit var binding: FragmentDataBinding
-//    private val viewModel : DataViewModel by viewModels() //fragment context
+
+    //    private val viewModel : DataViewModel by viewModels() //fragment context
 //    private val viewModel: DataViewModel by activityViewModels()
     private lateinit var viewModel: DataViewModel
 
@@ -29,10 +36,34 @@ class OneFragment : Fragment() {
         viewModel.number.observe(viewLifecycleOwner) {
             binding.tvValue.text = it.toString()
         }
+        viewModel.startActivity.observe(viewLifecycleOwner) {
+            if (it) {
+                val intent = Intent(requireContext(), MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+        viewModel.showToast.observe(viewLifecycleOwner) {
+            it.getContentIfNotHandled()?.let {
+                Toast.makeText(requireContext(), "toast context", Toast.LENGTH_LONG).show()
+            }
+        }
+//        lifecycleScope.launch {
+//            viewModel.toastChannel.collectLatest {
+//                Toast.makeText(requireContext(), "toast context", Toast.LENGTH_LONG).show()
+//            }
+//        }
+
         binding.btn.setOnClickListener {
             viewModel.updateNumber()
         }
 
+        binding.startActivty.setOnClickListener {
+            viewModel.startActivity()
+        }
+        binding.btnToastShow.setOnClickListener {
+            viewModel.showToast()
+        }
 
         return binding.root
     }
--- app/src/main/java/com/glucozo/android_jetpack/viewmodel/DataViewModel.kt
+++ app/src/main/java/com/glucozo/android_jetpack/viewmodel/DataViewModel.kt
@@ -1,12 +1,53 @@
 package com.glucozo.android_jetpack.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.glucozo.android_jetpack.util.Event
+import com.glucozo.android_jetpack.util.SingleLiveEvent
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
 
 class DataViewModel : ViewModel() {
-    var number = MutableLiveData<Int>(0)
+    //SAI CO CHE MVVM
+//    var number = MutableLiveData<Int>(0)
+//
+//    fun updateNumber(){
+//        number.value = number.value!!.plus(1)
+//    }
+
+
+    //co che backing field
+    private val _number = MutableLiveData(0)
+    val number : LiveData<Int>
+        get() = _number
+//one time event
+    private val _showToast = MutableLiveData<Event<Boolean>>()
+//    private val _showToast = MutableLiveData<Boolean>(false)
+//    val showToast : LiveData<Boolean>
+    val showToast : LiveData< Event<Boolean>>
+        get() = _showToast
+
+    private val _startActivity = SingleLiveEvent<Boolean>()
+//    private val _startActivity = MutableLiveData<Boolean>(false)
+    val startActivity : SingleLiveEvent<Boolean>
+//    val startActivity : LiveData<Boolean>
+        get() = _startActivity
+
+
+    private val _toastChannel = Channel<Boolean> {  }//onetime event co san cua kotlin
+    val toastChannel = _toastChannel.receiveAsFlow()
 
     fun updateNumber(){
-        number.value = number.value!!.plus(1)
+        _number.value = _number.value!!.plus(1)
     }
+
+    fun showToast(){
+        _showToast.postValue(Event(true))
+//        _showToast.postValue(false)
+    }
+    fun startActivity(){
+        _startActivity.value = true
+    }
+
 }
