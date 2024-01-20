c69621a2610dce11893f7bd99e4724737f726f97
ThaoDan2801 <thaoto2801@gmail.com>
2024-01-20 16:05:59 +0700

    update student manager with mvvm , viewModel, livedata, dataBinding

--- app/src/main/java/com/glucozo/android_jetpack/model/Student.kt
+++ app/src/main/java/com/glucozo/android_jetpack/model/Student.kt
@@ -7,7 +7,7 @@ data class Student(
     val avatar: String = ""
 ) {
     override fun equals(other: Any?): Boolean {
-        if (this == other) return true
+        if (this === other) return true
         if (javaClass != other?.javaClass) return false
 
         other as Student
